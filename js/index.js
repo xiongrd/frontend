0cfe8ad279e36725930405e069270f18ab77d6f9
RDX <xrd0312@outlook.com>
2021-09-12 22:21:05 +0800

    首次添加内容

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        firstCollections:
+        [
+            {title:'oimo.js',desc:"Lightweight 3d physics engine for javascript",url:'https://www.baidu.com'},
+            {title:'matter.js',desc:"Matter.js is a 2D physics engine for the web",url:'https://brm.io/matter-js/'},
+        ]
+    }
+})
