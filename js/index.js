973157b394e885968b98e64d489d8280b00253d7
RDX <xrd0312@outlook.com>
2021-09-14 22:54:06 +0800

    添加背景纹理，添加树形列表

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
+var treeData = {
+    name: "My Tree",
+    children: [
+      { name: "hello" },
+      { name: "wat" },
+      {
+        name: "child folder",
+        children: [
+          {
+            name: "child folder",
+            children: [{ name: "hello" }, { name: "wat" }]
+          },
+          { name: "hello" },
+          { name: "wat" },
+          {
+            name: "child folder",
+            children: [{ name: "hello" }, { name: "wat" }]
+          }
+        ]
+      }
+    ]
+  };
+
 var app = new Vue({
     el:'#app',
     data:{
+        treeData: treeData,
         firstCollections:
         [
             {title:'oimo.js',desc:"Lightweight 3d physics engine for javascript",url:'https://www.baidu.com'},
             {title:'matter.js',desc:"Matter.js is a 2D physics engine for the web",url:'https://brm.io/matter-js/'},
         ]
-    }
+    },
+    methods: {
+        makeFolder: function(item) {
+          Vue.set(item, "children", []);
+          this.addItem(item);
+        },
+        addItem: function(item) {
+          item.children.push({
+            name: "new stuff"
+          });
+        }
+      }
 })
